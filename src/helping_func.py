929f9fed61c69c166709ca5edf597282ab132df2
yahiarezk797 <yahiarezk797@gmail.com>
2026-01-12 18:42:32 +0200

    wy

--- src/helping_func.py
+++ src/helping_func.py
@@ -91,3 +91,9 @@ def text_to_textnodes(text):
     nodes = split_nodes_image(nodes)
     nodes = split_nodes_link(nodes)
     return nodes
+
+def markdown_to_blocks(markdown):
+    blocks = markdown.split("\n\n")
+    for i in range(len(blocks)):
+        blocks[i] = blocks[i].strip()
+    return blocks
--- src/try.py
+++ src/try.py
@@ -1,3 +1,11 @@
 from helping_func import *
 
-print(text_to_textnodes("This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"))
+markdown = """# This is a heading
+
+This is a paragraph of text. It has some **bold** and _italic_ words inside of it.
+
+- This is the first list item in a list block
+- This is a list item
+- This is another list item"""
+
+print(markdown_to_blocks(markdown))
